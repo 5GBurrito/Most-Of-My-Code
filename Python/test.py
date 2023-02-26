02eaf3065c336e02b9dafada350dd116b9636a54
5GBurrito <HuskyScout1405@gmail.com>
2023-02-26 09:01:27 -0600

    testing new ide

--- Python/test.py
+++ Python/test.py
@@ -1,7 +1,13 @@
-from random import randint
-from time import sleep
+# from random import randint
+# from time import sleep
 
-# Sleep a random number of seconds (between 1 and 5)
-print("start")
-sleep(randint(1,5))
-print("stop")
+# # Sleep a random number of seconds (between 1 and 5)
+# print("start")
+# sleep(randint(1,5))
+# # print("stop")
+# import pyttsx3
+#
+# text_speech = pyttsx3.init()
+# answer = input("what")
+# text_speech.say(answer)
+# text_speech.runAndWait
