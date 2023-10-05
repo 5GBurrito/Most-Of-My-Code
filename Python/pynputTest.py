5539de94137bf45261f0e0ed87915b7cde1ef6b7
Anthony <huskyscout1405@gmail.com>
2023-10-04 20:32:37 -0500

    I'm Back

---
+++ Python/pynputTest.py
@@ -0,0 +1,14 @@
+from pynput.keyboard import Key, Controller
+import time
+
+keyboard = Controller()
+
+time.sleep(3)
+
+for i in range(100):
+    for char in "":
+        keyboard.press(char)
+        keyboard.release(char)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(1)
