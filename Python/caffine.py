aee3556215c5aaa5f2aac9a6dbf5c8f2e15d8dea
5GBurrito <HuskyScout1405@gmail.com>
2024-03-20 10:40:05 -0500

    Refactored HTML and CSS code for Mom's Website. Added a banner, centered the title and subtitle, adjusted image sizes, added spacing between gallery images, and implemented a fade-in effect for images. additinoally, made a silly script for loud noises

---
+++ Python/caffine.py
@@ -0,0 +1,41 @@
+import random
+from time import sleep
+from playsound import playsound
+
+filePaths = [
+    "Python/SFX/-999SocialCreditSiren.mp3",
+    "Python/SFX/BassBoostedNFL.mp3",
+    "Python/SFX/DespicableMeWhistleBassBoosted.mp3",
+    "Python/SFX/DiscordPing.mp3",
+    "Python/SFX/FBIOPENUP.mp3",
+    "Python/SFX/TacoBellBong.mp3"
+]
+
+
+def selectSound():
+    currentSound = random.choice(filePaths)
+    return currentSound
+
+def safePlaysound(playWhat):
+    try:
+        playsound(playWhat)
+        print("Success! \n")
+    except:
+        print("Oops, it didn't work, hopefully this wont crash lolol")
+
+
+print("Beginning loop... \n")
+
+while True:
+    currentSound = selectSound()
+    print("Next sound: " + currentSound + "\n")
+
+    timer = 5
+    #timer = random.randint(30, 600)
+
+    for i in range(timer):
+        print("remaining time:", timer - i)
+        sleep(1)  # Sleep for 1 second
+
+    print("\nPlaying sound... \n")
+    safePlaysound(currentSound)
