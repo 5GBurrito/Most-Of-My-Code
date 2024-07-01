01f90f96f5558ce889e5dddd54e45bfddf46023c
5GBurrito <HuskyScot1405@gmail.com>
2024-07-01 14:46:44 -0600

    commented out test.java and did summer homework

---
+++ Random/7-1/summerHomework.py
@@ -0,0 +1,26 @@
+# Defnitions:
+#     Variables:
+#         values that can be changed throughout program's runtime
+#     User Input:
+#         Values or data inputed by the User of the program
+#     If Statements:
+#         A conditional statement that activates once if a condition is True
+#     Loop:
+#         Code that runs repeatdly for a  certian amount of time OR while a condition is true
+#     Print Statement:
+#         A statement that outputs data to the console
+
+#Lucky Number Generator
+print("welcome to the lucky number generator!")
+value1 = int(input("What day of the month is your birthday?     "))
+value2 = input("What did you eat for breakfast?     ")
+
+for i in range(value1):
+    value3 = len(value2)
+
+value4 = value1*value3
+
+if value4 < 10000:
+    print("Your lucky number is: ", value4)
+else:
+    print("Your lucky number is: ", value4%10000)
